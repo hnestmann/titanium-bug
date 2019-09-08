079804a00a13c28cf0e05f9680044a1409a9b3db
Holger Nestmann <hnestmann@salesforce.com>
2019-09-08 15:05:48 +0200

    simplified code

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,21 +3,9 @@ function doClick(e) {
 }
 var secondLoad = false;
 $.webview.addEventListener('load', function(e) {
-	Ti.API.warn('Loaded')
-	if (!secondLoad) {
-		Ti.API.warn('Change URL')
-		$.webview.reload();
-		secondLoad = true;
-	} else {
-		$.webview2.url = "https://hnestmann-inside-eu01-dw.demandware.net/on/demandware.store/Sites-RefArch-Site/en_US/CookieCheck-Check";
-		$.webview2.reload();
-	}
-});
+	$.webview2.url = "https://hnestmann-inside-eu01-dw.demandware.net/on/demandware.store/Sites-RefArch-Site/en_US/CookieCheck-Check";
+	$.webview2.reload();
 
-$.webview2.addEventListener('load', function(e) {
-	Ti.API.warn($.webview.evalJS("document.cookie"));
-	Ti.API.warn($.webview2.evalJS("document.cookie"));
 });
-
 
 $.index.open();
